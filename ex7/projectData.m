c4c618db4140764ba6e8dc48006e57806eb6cbdc
Amr Rady <amrrady1@gmail.com>
2021-02-05 16:20:03 +0100

    add solution to Assignment 7

--- ex7/projectData.m
+++ ex7/projectData.m
@@ -1,4 +1,3 @@
-function Z = projectData(X, U, K)
 %PROJECTDATA Computes the reduced data representation when projecting only 
 %on to the top k eigenvectors
 %   Z = projectData(X, U, K) computes the projection of 
@@ -7,20 +6,7 @@ function Z = projectData(X, U, K)
 %
 
 % You need to return the following variables correctly.
-Z = zeros(size(X, 1), K);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the projection of the data using only the top K 
-%               eigenvectors in U (first K columns). 
-%               For the i-th example X(i,:), the projection on to the k-th 
-%               eigenvector is given as follows:
-%                    x = X(i, :)';
-%                    projection_k = x' * U(:, k);
-%
-
-
-
-
-% =============================================================
-
+function Z = projectData(X, U, K)
+    U_reduce = U(:, 1:K);
+    Z = X * U_reduce;
 end
