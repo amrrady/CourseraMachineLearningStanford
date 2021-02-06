5eb29cdb4bb416889a896c4be94f10901c868855
Amr Rady <amrrady1@gmail.com>
2021-02-06 17:46:16 +0100

    add sol for assign 8

--- ex8/estimateGaussian.m
+++ ex8/estimateGaussian.m
@@ -14,23 +14,11 @@ function [mu sigma2] = estimateGaussian(X)
 mu = zeros(n, 1);
 sigma2 = zeros(n, 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the mean of the data and the variances
-%               In particular, mu(i) should contain the mean of
-%               the data for the i-th feature and sigma2(i)
-%               should contain variance of the i-th feature.
-%
-
-
-
-
-
-
-
-
-
-
-% =============================================================
+mu = 1/m * sum(X);
+for i= 1:n
+    sqDiff = (X(:,i) - mu(i)) .* (X(:,i) - mu(i));
+    sigma2(i) = 1/(m) * sum(sqDiff);
+end
 
 
 end
--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -1,17 +1,9 @@
-function [bestEpsilon bestF1] = selectThreshold(yval, pval)
 %SELECTTHRESHOLD Find the best threshold (epsilon) to use for selecting
 %outliers
 %   [bestEpsilon bestF1] = SELECTTHRESHOLD(yval, pval) finds the best
 %   threshold to use for selecting outliers based on the results from a
 %   validation set (pval) and the ground truth (yval).
 %
-
-bestEpsilon = 0;
-bestF1 = 0;
-F1 = 0;
-
-stepsize = (max(pval) - min(pval)) / 1000;
-for epsilon = min(pval):stepsize:max(pval)
     
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
@@ -22,20 +14,21 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
+function [bestEpsilon bestF1] = selectThreshold(yval, pval)
 
+bestEpsilon = 0;
+bestF1 = 0;
+F1 = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-    % =============================================================
+stepsize = (max(pval) - min(pval)) / 1000;
+for epsilon = min(pval):stepsize:max(pval)
+    predictions = (pval < epsilon);
+    tp = sum(predictions == 1 & yval == 1);
+    fp = sum(predictions == 1 & yval == 0);
+    fn = sum(predictions == 0 & yval == 1);
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
+    F1 = (2 * prec * rec) / ( prec + rec);
 
     if F1 > bestF1
        bestF1 = F1;
